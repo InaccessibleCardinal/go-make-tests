174720ddc3b2ef441fd33120a8ca7eae048eea5d
kenlandsbaum <kenneth.landsbaum@stellarelements.com>
2024-02-23 19:11:03 -0600

    chore

--- internal/ui/codeform.go
+++ internal/ui/codeform.go
@@ -1,8 +1,42 @@
 package ui
 
-import "fyne.io/fyne/v2/widget"
+import (
+	"go-make-tests/internal/svc/chat"
+	"log"
 
-func CreateCodeForm() *widget.Label {
-	label := widget.NewLabel("Under construction")
-	return label
+	"fyne.io/fyne/v2/widget"
+)
+
+type CodeFormDriver struct {
+	queryTextArea *widget.Entry
+	form          widget.Form
+	chatService   chat.ChatServiceIface
+}
+
+func (cf *CodeFormDriver) handleSubmit() {
+	log.Println("submitting", cf.queryTextArea.Text)
+	msg, err := cf.chatService.InvokeUserQuery(cf.queryTextArea.Text)
+	cf.queryTextArea.SetText("")
+	cf.queryTextArea.Refresh()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(msg.Content)
+}
+
+func NewCodeFormDriver(chatService chat.ChatServiceIface) *CodeFormDriver {
+	queryTextArea := widget.NewMultiLineEntry()
+	queryTextArea.SetMinRowsVisible(20)
+	queryTextArea.AcceptsTab()
+
+	return &CodeFormDriver{chatService: chatService, queryTextArea: queryTextArea, form: widget.Form{}}
+}
+
+func CreateCodeForm() *widget.Form {
+	cf := NewCodeFormDriver(chat.GetChatService())
+
+	cf.form.Append("Enter your query", cf.queryTextArea)
+	cf.form.OnSubmit = cf.handleSubmit
+	return &cf.form
 }
