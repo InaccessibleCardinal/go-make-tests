31a340ff29e08153d631ecfc0d98fd6d3de26c65
kenlandsbaum <kenneth.landsbaum@stellarelements.com>
2024-02-29 12:07:46 -0600

    chores

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"go-make-tests/internal/db"
 	"go-make-tests/internal/env"
-	creds "go-make-tests/internal/svc/credentials"
 	"go-make-tests/internal/ui"
-
-	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,19 +12,5 @@ func init() {
 }
 
 func main() {
-	// Connect()
 	ui.Run()
-
-}
-
-func Connect() {
-	db := db.NewCredsDB()
-	credsService := creds.New(db)
-
-	allCreds, err := credsService.GetAllCredentials()
-	env.LogFatalErr(err)
-
-	for _, c := range allCreds {
-		log.Printf("user: %s, access: %s, secret: %s\n", *c.User, *c.AccessKey, *c.SecretKey)
-	}
 }
